5b6215a19768d1b118ea2d0335201fb4024eaf13
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-03-27 17:25:18 -0400

    Add necessary parts for distribution

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fp:
+     long_description = fp.read()
 
 setup(
     name                 = "litei2c",
+    version              = "2024.12",
     description          = "Small footprint and configurable I2C core",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author               = "LiteI2C Developers",
     url                  = "https://github.com/litex-hub",
     download_url         = "https://github.com/litex-hub/litei2c",
@@ -15,6 +20,8 @@ setup(
     python_requires      = "~=3.7",
     packages             = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
     include_package_data = True,
+    install_requires=["migen", "litex"],
+    extras_require={"develop": ["setuptools"]},
     keywords             = "HDL ASIC FPGA hardware design",
     classifiers          = [
         "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
